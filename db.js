033d7782cf0bd81583f7c4c2e1a46d25e5aacec3
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-02-13 23:24:45 +0430

    Initial commit

---
+++ db.js
@@ -0,0 +1,199 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { existsSync } from 'node:fs';
+import {customAlphabet} from 'nanoid';
+
+// Define the character set for the IDs
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+// Create a custom nanoid function with a specific size
+export const getId = customAlphabet(alphabet, 8);
+
+export function createDb(adapter) {
+	return (collectionName) => {
+		return {
+			query({filters = [], page = 1, perPage= 0} = {}) {
+
+                return adapter.query(collectionName, {
+                    filters, 
+                    pagination: {page, perPage}
+                })
+			},
+			async insert(data) {
+                data.id ??= getId();
+                const result = await adapter.insert(collectionName, data);
+				return result;
+			},
+			async remove(id) {
+                await adapter.remove(collectionName, id)
+                return true;
+			},
+			async update(id, data) {
+				const result = await adapter.update(collectionName, id, data);
+                return result
+			}
+		};
+	};
+}
+
+function applyComparison(value, operator, compareValue) {
+    switch (operator) {
+        case '=':
+            return value === compareValue;
+        case '<':
+            return value < compareValue;
+        case '<=':
+            return value <= compareValue;
+        case '>':
+            return value > compareValue;
+        case '>=':
+            return value >= compareValue;
+        // Add other conditions as needed
+        default:
+            return true; // No comparison applied for unknown operators
+    }
+}
+
+function applyFilters(items, filters) {
+    return filters.reduce((prev, curr) => {
+        return prev.filter((x) => applyComparison(x[curr.field], curr.operator, curr.value));
+    }, items);
+}
+
+let db = {};
+
+export const createAdapter1 = () => {
+    return {
+        insert(collection, data) {
+            if (!db[collection]) {
+                db[collection] = [];
+            }
+            db[collection].push(data);
+            return data;
+        },
+
+        query(collection, {pagination = {page: 1, perPage: 0}, filters = []}) {
+            if (!db[collection]) {
+                return {data:[], total: 0, page: 1, perPage: 0};
+            }
+
+            let items = applyFilters(db[collection], filters)
+
+            return {
+                data: pagination.perPage === 0 ? items : items.slice(
+                    (pagination.page - 1) * pagination.perPage,
+                    pagination.page * pagination.perPage
+                ),
+                total: items.length,
+                page: pagination.page,
+                perPage: pagination.perPage === 0 ? items.length : Math.min(items.length, pagination.perPage)
+            }
+        },
+
+        update(collection, id, data) {
+            if (!db[collection]) {
+                return null;
+            }
+            const index = db[collection].findIndex(item => item.id === id);
+            if (index !== -1) {
+                db[collection][index] = { ...db[collection][index], ...data };
+                return db[collection][index];
+            }
+            return null;
+        },
+
+        remove(collection, id) {
+            if (!db[collection]) {
+                return null;
+            }
+            const index = db[collection].findIndex(item => item.id === id);
+            if (index !== -1) {
+                const deleted = db[collection][index];
+                db[collection].splice(index, 1);
+                return deleted;
+            }
+            return null;
+        }
+    }
+}
+
+export const createAdapter = (token) => {
+    if(!existsSync('./data/' + token + '/db.json')) {
+        throw new Error('database not esists. create new database first (/new)')
+    }
+    let db = {}
+
+    function read() {
+        const file = readFileSync('./data/' + token + '/data.json');
+        db = JSON.parse(file)
+    }
+    read()
+
+    function write() {
+        writeFileSync('./data/' + token + '/data.json', JSON.stringify(db));
+    }
+
+    let isDirty = false;
+
+    setInterval(() => {
+        if(isDirty) {
+            write()
+        }
+        isDirty = false
+    }, 2000)
+
+    return {
+        insert(collection, data) {
+            if (!db[collection]) {
+                db[collection] = [];
+            }
+            db[collection].push(data);
+            isDirty = true
+            return data;
+        },
+
+        query(collection, {pagination = {page: 1, perPage: 0}, filters = []}) {
+            if (!db[collection]) {
+                return {data:[], total: 0, page: 1, perPage: 0};
+            }
+
+            let items = applyFilters(db[collection], filters)
+
+            return {
+                data: pagination.perPage === 0 ? items : items.slice(
+                    (pagination.page - 1) * pagination.perPage,
+                    pagination.page * pagination.perPage
+                ),
+                total: items.length,
+                page: pagination.page,
+                perPage: pagination.perPage === 0 ? items.length : Math.min(items.length, pagination.perPage)
+            }
+        },
+
+        update(collection, id, data) {
+            if (!db[collection]) {
+                return null;
+            }
+            const index = db[collection].findIndex(item => item.id === id);
+            if (index !== -1) {
+                db[collection][index] = { ...db[collection][index], ...data };
+                isDirty = true
+                return db[collection][index];
+            }
+            return null;
+        },
+
+        remove(collection, id) {
+            if (!db[collection]) {
+                return null;
+            }
+            const index = db[collection].findIndex(item => item.id === id);
+            if (index !== -1) {
+                const deleted = db[collection][index];
+                db[collection].splice(index, 1);
+                isDirty = true
+                return deleted;
+            }
+            return null;
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,76 @@
+import express from 'express'
+import { createAdapter, createDb, getId } from './db.js'
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.end('Svelite Db is running!')   
+})
+
+app.post('/:token/:table/query', (req, res) => {
+    // validate token
+    const {token, table} = req.params
+    const {filters, sort, page, perPage} = req.body
+
+    const db = createDb(createAdapter(token))
+
+
+    // filters = [{key: 'title', operator: '=', value: 'something'}]
+    // sort = {column: 'title', order: 'ASC'}
+    
+    const result = db(table).query({filters, page, perPage})
+    
+    return res.json(result)
+})
+
+app.post('/:token/:table/insert', (req, res) => {
+    // validate token
+    const {token, table} = req.params
+    const data = req.body
+    
+    const result = db(table).insert(data)
+    
+    return res.json(result)
+})
+
+app.post('/:token/:table/update', (req, res) => {
+    // validate token
+    const {token, table} = req.params
+    const data = req.body
+    
+    const result = db(table).update(data.id, data)
+    
+    return res.json(result)
+})
+
+app.post('/:token/:table/remove', (req, res) => {
+    // validate token
+    const {token, table} = req.params
+    const {id} = req.body
+    
+    const result = db(table).remove(id)
+    
+    return res.json(result)
+})
+
+app.post('/new', (req, res) => {
+    const token = 'sv-' + getId() + '-' + getId() + '-' + getId()
+
+    if(!existsSync('./data')) {
+        mkdirSync('./data');
+    }
+
+    mkdirSync('./data/' + token)
+    writeFileSync('./data/' + token + '/db.json', '{}');
+    
+    res.json({
+        token
+    })
+})
+
+const {PORT = 3000} = process.env
+
+app.listen(PORT, () => console.log('App started on http://localhost:' + PORT))
